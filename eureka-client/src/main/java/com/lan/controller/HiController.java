2c32eeac2ca14b3293e4d60cfd2fb3bc86333bd0
lanmaozhi <19802021372@139.com>
2020-09-10 11:31:15 +0800

    引入zipkin及其它一些杂项提交

--- eureka-client/src/main/java/com/lan/controller/HiController.java
+++ eureka-client/src/main/java/com/lan/controller/HiController.java
@@ -1,18 +1,42 @@
 package com.lan.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @RestController
 public class HiController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HiController.class);
 
     @Value("${server.port}")
     String port;
 
+    @Resource
+    private EurekaDiscoveryClient discoveryClient;
+
     @GetMapping("/hi")
     public String home(@RequestParam(value = "name", defaultValue = "lanmz") String name) {
+
+        logger.info("info");
         return "hi " + name + " ,i am from port:" + port;
+    }
+
+    @GetMapping("/hello")
+    public String sayHello() {
+
+        String description = discoveryClient.description();
+        List<String> services = discoveryClient.getServices();
+        List<ServiceInstance> instances = discoveryClient.getInstances("service-hi");
+
+        return "hello";
     }
 }
--- service-ribbon/src/main/java/com/lan/service/impl/HelloServiceImpl.java
+++ service-ribbon/src/main/java/com/lan/service/impl/HelloServiceImpl.java
@@ -2,6 +2,8 @@ package com.lan.service.impl;
 
 import com.lan.service.IHelloService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,6 +12,8 @@ import javax.annotation.Resource;
 @Service
 public class HelloServiceImpl implements IHelloService {
 
+    private static final Logger logger = LoggerFactory.getLogger(HelloServiceImpl.class);
+
     @Resource
     private RestTemplate restTemplate;
 
@@ -17,6 +21,7 @@ public class HelloServiceImpl implements IHelloService {
     @HystrixCommand(fallbackMethod = "hiError")
     public String sayHi(String name) {
 
+        logger.info("info");
         return restTemplate.getForObject("http://SERVICE-HI/hi?name=" + name, String.class);
     }
 
